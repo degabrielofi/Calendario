4dbd79c24ef4030d279f6a8b0c8c1cf7c4a1b08e
Gabriel Silva <yzgabrieel02@gmail.com>
2022-04-20 02:01:11 -0300

    Calendário Brasileiro

--- script.js
+++
@@ -1,9 +0,0 @@
-const DiaNumero = new Date().getDate();
-const Ano = new Date().getFullYear();
-const DiaNome = new Date().toLocaleString("default", {weekday: "long"});
-const MesNome = new Date().toLocaleString("default", {month: "long"});
-
-document.querySelector(".mês-nome").innerHTML = MesNome;
-document.querySelector(".dia-nome").innerHTML = DiaNome;
-document.querySelector(".data-numero").innerHTML = DiaNumero;
-document.querySelector(".ano").innerHTML = Ano;
